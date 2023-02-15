44e084810b0e039bd2c36158282e2603c09ce4ff
chorimgenius <artyum15@kakao.com>
2023-02-16 02:09:29 +0900

    Feat: user password validation 설정 #1
    - 비밀번호 제약 조건 설정(8글자 이상, 특수문자, 숫자 포함)

--- user/serializers.py
+++ user/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from user.models import User
+import re 
+
 
 class CustomUserserializer(serializers.ModelSerializer):
-    
+
     class Meta:
-        model=User
+        model = User
         fields = "__all__"
+
+    def validate_password(self, value):
+        pattern = r'^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z]).{8,}$'
+        if not re.search(pattern, value):
+            raise serializers.ValidationError('비밀번호는 8글자 이상이며 특수문자, 숫자를 포함해야 합니다.')
+        return value
         
 class CustomObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
