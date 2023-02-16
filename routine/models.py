72d251410cd77757ba63902773ed8f179a117925
chorimgenius <artyum15@kakao.com>
2023-02-16 23:39:50 +0900

    Feat: Routine 모델 생성 #2

--- routine/models.py
+++ routine/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from user.models import User
+
+class Routine(models.Model):
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="제목",max_length=30, unique=True)
+    category_choice = (
+        ('MIRACLE', 'MIRACLE'),
+        ('HOMEWORK', 'HOMEWORK')
+    )
+    category = models.CharField(verbose_name="카테고리",max_length=15, choices=category_choice)
+    goal = models.CharField(verbose_name="목표",max_length=40)
+    is_alarm = models.BooleanField(verbose_name="알람여부",max_length=15)
+    is_deleted = models.BooleanField(verbose_name="삭제여부",max_length=10, default=False)
+    created_at = models.DateTimeField(verbose_name="생성시간",auto_now_add = True)
+    modified_at = models.DateTimeField(verbose_name="수정시간",auto_now = True)
