94f996866dc4e7df1c9c11c3e4989e3a4b184a3c
chorimgenius <artyum15@kakao.com>
2023-02-18 16:36:10 +0900

    Feat: routine url 패턴 생성  #2
    - routine, rountinelist, routinedetail url 패턴 생성

--- routine/urls.py
+++ routine/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from routine.views import views
+from routine import views
 
 urlpatterns = [
-    
+    path('create/', views.RoutineCreateview.as_view(), name="routine"),
+    path('routinelist/<int:account_id>/', views.RoutineLListView.as_view(), name="routinelist"),
+    path('<int:account_id>/<int:routine_id>/', views.RoutineDetailview().as_view(), name="routinedetail"),
 ]
