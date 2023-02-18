ead341eb99cea59782e2ba6686c192d18482b425
chorimgenius <artyum15@kakao.com>
2023-02-18 16:33:26 +0900

    Feat: routine view 구조 생성 #2
    - RoutineCreateView - POST
    - RoutineLstView  = GET
    - RoutineDetailView - GET, PUT, DELETE

--- routine/views.py
+++ routine/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView, Listview
+from rest_framework.response import Response
+from routine.models import Routine, RoutineDay, RoutineResult
 
-# Create your views here.
+class RoutineCreateView(APIView):
+    def post(self, request):
+        pass
+    
+class RoutineListView(APIView):
+    def get(self, request):
+        pass
+
+class RoutineDetailView(APIView):
+    def get(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
+
