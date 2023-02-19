761cc701d0fbb10ea51497a21928dc1535139cde
chorimgenius <artyum15@kakao.com>
2023-02-19 23:53:15 +0900

    Fix: routine url 오타 수정 #2
    - RoutineCreateview -> RoutineCreateView로 수정

--- routine/urls.py
+++ routine/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from routine import views
 
 urlpatterns = [
-    path('create/', views.RoutineCreateview.as_view(), name="routine"),
-    path('routinelist/<int:account_id>/', views.RoutineLListView.as_view(), name="routinelist"),
-    path('<int:account_id>/<int:routine_id>/', views.RoutineDetailview().as_view(), name="routinedetail"),
+    path('create/', views.RoutineCreateView.as_view(), name="routine"),
+    path('routinelist/<int:account_id>/', views.RoutineListView.as_view(), name="routinelist"),
+    path('<int:account_id>/<int:routine_id>/', views.RoutineDetailView.as_view(), name="routinedetail"),
 ]
