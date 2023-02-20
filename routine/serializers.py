ff695dc60869b54dcc3c1cd31e0fbb2448cae9b9
chorimgenius <artyum15@kakao.com>
2023-02-21 02:45:54 +0900

    Feat: RoutineResultSerializer, RoutineListSerializer 추가 #2

--- routine/serializers.py
+++ routine/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from routines.models import Routine, RoutineDay, RoutineResult
+from routine.models import Routine, RoutineDay, RoutineResult
 
 class RoutineCreateSerializer(serializers.ModelSerializer):
     days = serializers.ListField()
@@ -13,3 +13,14 @@ class RoutineCreateSerializer(serializers.ModelSerializer):
         for day in days_list:
             RoutineDay.objects.create(day=int(day), routine=routine)
         return routine
+
+class RoutineResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoutineResult
+        fields = ('result',)
+
+class RoutineListSerializer(serializers.ModelSerializer):
+    result = RoutineResultSerializer(many=True)
+    class Meta:
+        model = Routine
+        fields = ('goal','account_id','result', 'title')
