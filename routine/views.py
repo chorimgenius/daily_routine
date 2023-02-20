af6f61f90d413ecf73418b1b19ca479d9d9e42f1
chorimgenius <artyum15@kakao.com>
2023-02-20 16:32:33 +0900

    Fix: import 수정  #2
    - Listview 삭제

--- routine/views.py
+++ routine/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.views import APIView, Listview
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from routine.models import Routine, RoutineDay, RoutineResult
 
