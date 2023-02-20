cedd63b351438fbaface40ee2c5c434b064ff696
chorimgenius <artyum15@kakao.com>
2023-02-21 02:38:20 +0900

    Feat: user serializer 비밀번호 해싱 기능 추가  #1
    - CustomUserserializer create 함수 추가

--- user/serializers.py
+++ user/serializers.py
@@ -9,12 +9,20 @@ class CustomUserserializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
 
     def validate_password(self, value):
         pattern = r'^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z]).{8,}$'
         if not re.search(pattern, value):
             raise serializers.ValidationError('비밀번호는 8글자 이상이며 특수문자, 숫자를 포함해야 합니다.')
         return value
+
         
 class CustomObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
