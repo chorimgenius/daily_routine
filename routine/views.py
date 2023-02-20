ea9cc9ea13ba200a0a80fc17e9251386129a297e
chorimgenius <artyum15@kakao.com>
2023-02-21 02:46:44 +0900

    Feat: RoutineCreateView, RoutineListView 내용 작성 #2

--- routine/views.py
+++ routine/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Q
 from routine.models import Routine, RoutineDay, RoutineResult
-from routine.serializers import RoutineCreateSerializer
+from routine.serializers import RoutineCreateSerializer, RoutineListSerializer
 
 class RoutineCreateView(APIView):
     def post(self, request):
@@ -11,14 +12,38 @@ class RoutineCreateView(APIView):
         data["days"] = [DAYS_DICT[x] for x in data.get("days", [])]
         serializer = RoutineCreateSerializer(data = data)
         if serializer.is_valid():
-            serializer.save(account_id = request.user.id)
-            return Response({"msg": "You have successfully created the routine."}, status=status.HTTP_201_CREATED)
+            routine = serializer.save(account_id = request.user.id)
+            routine_id = routine.id
+            message = {
+            "msg": "You have successfully created the routine",
+            "status": "ROUTINE_CREATED_OK"
+            }
+            data = {
+                "routine_id": routine_id
+            }
+            response_data = {
+                "data": data,
+                "message": message
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class RoutineListView(APIView):
-    def get(self, request):
-        pass
+    def get (self, request, account_id):
+        today = request.data['today']
+        routines = Routine.objects.filter(Q(account_id=account_id)|Q(created_at=today))
+        serializer = RoutineListSerializer(routines, many=True)
+        data = serializer.data
+        message = {
+            "msg": "Routine lookup was successful",
+            "status": "ROUTINE_LIST_OK"
+        }
+        response_data = {
+                "data": data,
+                "message": message
+            }
+        return Response(response_data, status=status.HTTP_200_OK)
 
 class RoutineDetailView(APIView):
     def get(self, request):
