25b0d351f31fe3d00aed23a2929ce82f75135bbe
chorimgenius <artyum15@kakao.com>
2023-02-24 00:18:07 +0900

    Feat: RoutineDetailView 생성  #2
    - get, post, delete 함수 작성

--- routine/views.py
+++ routine/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from django.db.models import Q
 from routine.models import Routine, RoutineDay, RoutineResult
-from routine.serializers import RoutineCreateSerializer, RoutineListSerializer
+from routine.serializers import RoutineCreateSerializer, RoutineListSerializer, RoutineDetailSerializer
 
 class RoutineCreateView(APIView):
     def post(self, request):
@@ -32,9 +33,11 @@ class RoutineCreateView(APIView):
 class RoutineListView(APIView):
     def get (self, request, account_id):
         today = request.data['today']
+        print(today)
         routines = Routine.objects.filter(Q(account_id=account_id)|Q(created_at=today))
         serializer = RoutineListSerializer(routines, many=True)
         data = serializer.data
+        print(data)
         message = {
             "msg": "Routine lookup was successful",
             "status": "ROUTINE_LIST_OK"
@@ -46,10 +49,62 @@ class RoutineListView(APIView):
         return Response(response_data, status=status.HTTP_200_OK)
 
 class RoutineDetailView(APIView):
-    def get(self, request):
-        pass
-    def put(self, request):
-        pass
-    def delete(self, request):
-        pass
+    def get(self, request, account_id, routine_id):
+        routine = get_object_or_404(Routine, account_id=account_id, id = routine_id)
+        serializer = RoutineDetailSerializer(routine)
+        data = serializer.data
+        message = {
+            "msg": "Routine lookup was successful",
+            "status": "ROUTINE_LIST_OK"
+        }
+        response_data = {
+                "data": data,
+                "message": message
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    # 수정  
+    def put(self, request, account_id, routine_id):
+        routine = get_object_or_404(Routine, account_id=account_id, id = routine_id)
+        if request.user.id == account_id:
+            serializer = RoutineSerializer(routine, data=request.data, partial=True)
+            if serializer.is_valid():
+                routine = serializer.save()
+                data = routine.id
+                message = {
+                    "msg": "The routine has been modified.",
+                    "status": "ROUTINE_UPDATED_OK"
+                }
+                data = {
+                    "routine_id": routine_id
+                }
+                response_data = {
+                    "data": data,
+                    "message": message
+                }
+                return Response(response_data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "수정 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+    #삭제
+    def delete(self, request, account_id, routine_id):
+        routine = get_object_or_404(Routine, account_id=account_id, id = routine_id)
+        if request.user.id == account_id:
+            routine.delete()
+            data = {
+                "routine_id": id
+            }
+            message = {
+                    "msg": "The routine has been deleted.",
+                    "status": "ROUTINE_DELETE_OK"
+            }
+            response_data = {
+                    "data": data,
+                    "message": message
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "작성자가 아닙니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
